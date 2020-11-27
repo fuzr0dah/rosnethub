7161e2bc30026627f9183e34f0e5b8379611d4ff
fuzr0dah <0fateful0@gmail.com>
2020-11-27 20:00:52 +0300

    Initial Commit

---
+++ rosnethub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rosnethub
+//
+//  Created by Артём Поляничев on 27.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
